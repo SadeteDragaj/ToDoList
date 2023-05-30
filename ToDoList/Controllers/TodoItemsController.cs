6fe98882cc2843795f0e53603bd0dae9f5b121eb
SadeteDragaj <81782815+SadeteD@users.noreply.github.com>
2023-05-30 16:31:34 +0200

    ItemController

--- ToDoList/Controllers/TodoItemsController.cs
+++ ToDoList/Controllers/TodoItemsController.cs
@@ -49,6 +49,55 @@ namespace ToDoList.Controllers
 
             return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.ID },todoItem);
         }
-      
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
+        {
+            if (id != todoItem.ID)
+            {
+                return BadRequest();
+            }
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemAvailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+
+            }
+            return Ok();
+        }
+            private bool TodoItemAvailable(int id)
+            {
+                return (_context.TodoItems?.Any(x => x.ID == id)).GetValueOrDefault();
+            }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(int id)
+        {
+            if(_context.TodoItems == null)
+            {
+                return NotFound();
+            }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+           _context.TodoItems.Remove(todoItem);
+
+            await _context.SaveChangesAsync();
+            return Ok(); 
+        }
+        }
     }
-}
+
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
